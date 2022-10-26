9b09305e179c7f3414d8ac27f9142490438851aa
FluffyCube9343 <learningninja007@gmail.com>
2022-10-25 22:20:15 -0400

    order matters

--- components/attribute.js
+++ components/attribute.js
@@ -1,9 +1,9 @@
 const Attribute = (props) => {
     return (
         <div className="transition ease-in-out hover:-translate-y-2 hover:scale-105 duration-300 underline decoration-indigo-500 underline-offset-8 decoration-4 pt-16 pb-16 flex flex-row items-center whitespace-nowrap">
-            <h1 className="attribute1 text-white text-2xl">{props.first}</h1>
-            <h1 className="attribute1 pl-4 pr-4 text-white text-2xl">-</h1>
-            <h1 className="attribute1 text-white text-2xl">{props.second}</h1>
+            <h1 className=" text-white text-2xl attribute1">{props.first}</h1>
+            <h1 className=" pl-4 pr-4 text-white text-2xl attribute1">-</h1>
+            <h1 className=" text-white text-2xl attribute1">{props.second}</h1>
         </div>
     );
 };
