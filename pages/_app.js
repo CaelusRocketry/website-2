19a782936e991a98fcf6d0604877a98b65dcd227
FluffyCube9343 <learningninja007@gmail.com>
2022-10-29 15:52:12 -0400

    revert2

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
-import Navbar from "../components/navbar"
-import "../styles/globals.css"
+import Navbar from "../components/navbar";
+import "../styles/globals.css";
+import "../javascript/jsstuff.js";
 
 function MyApp({ Component, pageProps }) {
-  return <>
-    <Navbar />
+    return (
+        <>
+            <Navbar />
 
-    <Component {...pageProps} />
-  </>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
